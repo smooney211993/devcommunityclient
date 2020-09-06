9a4a692173f9d101afbf3b81e9a3f244f0587280
smooney211993 <sm.athome21@gmail.com>
2020-09-06 11:32:55 +1000

    created react routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from './Components/layout/Navbar';
 import Landing from './Components/layout/Landing';
+import Register from './Components/auth/Register';
+import Login from './Components/auth/Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
@@ -9,8 +11,13 @@ const App = () => {
     <Router>
       <>
         <Navbar />
-        <Landing />
         <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
       </>
     </Router>
   );
