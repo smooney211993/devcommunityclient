4b9f1434f7d8b469707c8868b04f437b72aab631
smooney211993 <sm.athome21@gmail.com>
2020-09-06 11:06:32 +1000

    created register and login components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import Navbar from './Components/layout/Navbar';
+import Landing from './Components/layout/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <Router>
+      <>
+        <Navbar />
+        <Landing />
+        <Route exact path='/' component={Landing} />
+      </>
+    </Router>
   );
 };
 export default App;
