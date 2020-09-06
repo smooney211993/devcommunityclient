1df06135b4e97de612880291fdd757ab7503be0c
smooney211993 <sm.athome21@gmail.com>
2020-09-06 12:27:05 +1000

    created the basic functionality of user registration

--- src/Components/auth/Login.js
+++ src/Components/auth/Login.js
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = formState;
+  const handleFormState = (event) =>
+    setFormState({ ...formState, [event.target.name]: event.target.value });
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await fetch('http://localhost:3001/api/users', {
+        method: 'POST',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      if (data.ok) {
+        const jsonResponse = data.json();
+        console.log(jsonResponse);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Create Your Account
+      </p>
+      <form className='form' action='create-profile.html' onSubmit={onSubmit}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            onChange={handleFormState}
+            value={email}
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            onChange={handleFormState}
+            value={password}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <a href='login.html'>Sign In</a>
+      </p>
+    </>
+  );
 };
 
 export default Login;
--- src/Components/auth/Register.js
+++ src/Components/auth/Register.js
@@ -1,46 +1,96 @@
 import React, { useState } from 'react';
 
 const Register = () => {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+  const { name, email, password, password2 } = formState;
+  const handleFormState = (event) =>
+    setFormState({ ...formState, [event.target.name]: event.target.value });
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('password does not match');
+    }
+    try {
+      const data = await fetch('http://localhost:3001/api/users', {
+        method: 'POST',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+      if (data.ok) {
+        const jsonResponse = data.json();
+        console.log(jsonResponse);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-      <section class='container'>
-        <h1 class='large text-primary'>Sign Up</h1>
-        <p class='lead'>
-          <i class='fas fa-user'></i> Create Your Account
-        </p>
-        <form class='form' action='create-profile.html'>
-          <div class='form-group'>
-            <input type='text' placeholder='Name' name='name' required />
-          </div>
-          <div class='form-group'>
-            <input type='email' placeholder='Email Address' name='email' />
-            <small class='form-text'>
-              This site uses Gravatar so if you want a profile image, use a
-              Gravatar email
-            </small>
-          </div>
-          <div class='form-group'>
-            <input
-              type='password'
-              placeholder='Password'
-              name='password'
-              minLength='6'
-            />
-          </div>
-          <div class='form-group'>
-            <input
-              type='password'
-              placeholder='Confirm Password'
-              name='password2'
-              minLength='6'
-            />
-          </div>
-          <input type='submit' class='btn btn-primary' value='Register' />
-        </form>
-        <p class='my-1'>
-          Already have an account? <a href='login.html'>Sign In</a>
-        </p>
-      </section>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Create Your Account
+      </p>
+      <form className='form' action='create-profile.html' onSubmit={onSubmit}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            required
+            onChange={handleFormState}
+            value={name}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            onChange={handleFormState}
+            value={email}
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            onChange={handleFormState}
+            value={password}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            minLength='6'
+            onChange={handleFormState}
+            value={password2}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <a href='login.html'>Sign In</a>
+      </p>
     </>
   );
 };
