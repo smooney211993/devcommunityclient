0c9f40bd9fd9c0d5e92688c17eb2056d34925ae8
smooney211993 <sm.athome21@gmail.com>
2020-09-07 14:50:34 +1000

    fixed the routing

--- src/Components/layout/Landing.js
+++ src/Components/layout/Landing.js
@@ -11,10 +11,10 @@ const Landing = () => {
             other developers
           </p>
           <div className='buttons'>
-            <Link to='register' className='btn btn-primary'>
+            <Link to='/register' className='btn btn-primary'>
               Sign Up
             </Link>
-            <Link to='login' className='btn btn-light'>
+            <Link to='/login' className='btn btn-light'>
               Login
             </Link>
           </div>
--- src/Components/layout/Navbar.js
+++ src/Components/layout/Navbar.js
@@ -5,19 +5,19 @@ const Navbar = () => {
   return (
     <nav className='navbar bg-dark'>
       <h1>
-        <Link to='index.html'>
+        <Link to='/'>
           <i className='fas fa-code'></i> DevCommunity
         </Link>
       </h1>
       <ul>
         <li>
-          <Link to='profiles'>Developers</Link>
+          <Link to='/profiles'>Developers</Link>
         </li>
         <li>
-          <Link to='register'>Register</Link>
+          <Link to='/register'>Register</Link>
         </li>
         <li>
-          <Link to='login'>Login</Link>
+          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </nav>
