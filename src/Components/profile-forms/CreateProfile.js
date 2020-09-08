47147813354b4823a98abdf35cf118da859b8edf
smooney211993 <sm.athome21@gmail.com>
2020-09-08 13:07:13 +1000

    createprofile component added

---
+++ src/Components/profile-forms/CreateProfile.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const CreateProfile = (props) => {
+  const [formState, setFormState] = useState();
+  return <div></div>;
+};
+
+CreateProfile.propTypes = {};
+
+export default CreateProfile;
