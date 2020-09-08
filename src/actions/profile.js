9b3f6b4f619ead0979227f997473cd8fca76f3c8
smooney211993 <sm.athome21@gmail.com>
2020-09-08 20:52:24 +1000

    added in update education and experience action

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
 // get current user profile
 export const getCurrentProfile = () => async (dispatch) => {
   try {
@@ -40,6 +40,72 @@ export const createProfile = (formState, history, edit = false) => async (
     if (!edit) {
       history.push('/dashboard');
     }
+    // alerts the user that profile is created and redirects them to dashboard
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+export const addExperience = (formState, history) => async (dispatch) => {
+  try {
+    const config = {
+      headers: { 'Content-type': 'application/json' },
+    };
+    const { data } = await axios.put(
+      `http://localhost:3001/api/profile/experience`,
+      formState,
+      config
+    );
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: data,
+    });
+    dispatch(setAlert('Experience updated', 'success'));
+    history.push('/dashboard');
+
+    // alerts the user that profile is created and redirects them to dashboard
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+export const addEducation = (formState, history) => async (dispatch) => {
+  try {
+    const config = {
+      headers: { 'Content-type': 'application/json' },
+    };
+    const { data } = await axios.put(
+      `http://localhost:3001/api/profile/education`,
+      formState,
+      config
+    );
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: data,
+    });
+    dispatch(setAlert('Education updated', 'success'));
+    history.push('/dashboard');
+
     // alerts the user that profile is created and redirects them to dashboard
   } catch (err) {
     const errors = err.response.data.errors;
