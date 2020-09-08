7ef8983c817c6af9845b6afcef41d459620b8635
smooney211993 <sm.athome21@gmail.com>
2020-09-08 10:24:50 +1000

    added in comments

--- src/Components/routing/PrivateRoute.js
+++ src/Components/routing/PrivateRoute.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
+// Returning a route with a component that is passed(dashboard) as a render prop if it is loading and authenticated
+// it will has the component passed in and all of the other props
 const PrivateRoute = ({
   component: Component,
   auth: { isAuthenticated, loading },
